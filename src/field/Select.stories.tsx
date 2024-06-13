7dee81b035b327ff0c9562dcadfd1fd2ea3fbbf5
Reiju Takahashi <reizt.dev@gmail.com>
2024-06-13 16:31:05 +0900

    add Select

---
+++ src/field/Select.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Select } from './Select';
+
+export default {
+	title: 'Select',
+	component: Select,
+} satisfies Meta<typeof Select>;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {
+	args: {
+		children: (
+			<>
+				<option key="1">Option 1</option>
+				<option key="2">Option 2</option>
+				<option key="3">Option 3</option>
+			</>
+		),
+	},
+};
---
+++ src/field/Select.tsx
@@ -0,0 +1,11 @@
+import { clsx } from 'clsx';
+
+type Props = React.SelectHTMLAttributes<HTMLSelectElement>;
+
+export function Select({ className, children, ...rest }: Props): React.ReactNode {
+	return (
+		<select {...rest} className={clsx('h-44 rounded-10 border-2 border-gray-ee bg-gray-f7 px-16 placeholder:text-gray-aa', className)}>
+			{children}
+		</select>
+	);
+}
